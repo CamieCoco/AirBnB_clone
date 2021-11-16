9a0b0374beb2c55a8c01d12ddd6d742a88101796
CamieCoco <75216613+CamieCoco@users.noreply.github.com>
2021-11-17 00:48:20 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,53 +1,77 @@
 #!/usr/bin/python3
-"""
-Contains class BaseModel
-"""
-
-from datetime import datetime
-from uuid import uuid4
+'''
+Defines the class BaseModel
+'''
+import uuid
 import models
+from datetime import datetime
 
 
 class BaseModel:
-    """The BaseModel class from whichthe other classes will be derived"""
+    '''Defines all common attributes/methods for other classes
+    Attributes:
+        id (string): unique user id
+        created_at (datetime): time object was created
+        updated__at (datetime): time object was  changed
+    '''
 
     def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
+        '''Initializes an object
+        '''
+        time_format = '%Y-%m-%dT%H:%M:%S.%f'
         if kwargs:
-            for key, val in kwargs.items():
-                if "created_at" == key:
-                    self.created_at = datetime.strptime(kwargs["created_at"],
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                elif "updated_at" == key:
-                    self.updated_at = datetime.strptime(kwargs["updated_at"],
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                elif "__class__" == key:
+            for k, v in kwargs.items():
+                if k == "__class__":
                     pass
                 else:
-                    setattr(self, key, val)
+                    setattr(self, k, v)
+            if 'id' in kwargs.keys():
+                self.id = kwargs['id']
+            if 'created_at' in kwargs.keys():
+                self.created_at = datetime.strptime(kwargs['created_at'],
+                                                    time_format)
+            if 'updated_at' in kwargs.keys():
+                self.updated_at = datetime.strptime(kwargs['updated_at'],
+                                                    time_format)
+
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
 
     def __str__(self):
-        """String representation of the BaseModel class"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                         self.__dict__)
+        '''Prints a string version of any object
+        Return:
+            returns the string version
+        '''
+        return '[{}] ({}) {}'.format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """updates the attribute 'updated_at' with the current datetime"""
+        '''Updates updated_at with the current datetime
+        '''
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
-        time = "%Y-%m-%dT%H:%M:%S.%f"
-        dic = self.__dict__.copy()
-        if "created_at" in dic:
-            dic["created_at"] = dic["created_at"].strftime(time)
-        if "updated_at" in dic:
-            dic["updated_at"] = dic["updated_at"].strftime(time)
-        dic["__class__"] = self.__class__.__name__
-        return dic
+        '''Returns a dictionary of all keys/values of __dict__
+        Return:
+            returns a dictionary
+        '''
+        self_dict = self.__dict__.copy()
+        self_dict['__class__'] = self.__class__.__name__
+        self_dict['created_at'] = self.created_at.isoformat()
+        self_dict['updated_at'] = self.updated_at.isoformat()
+        return self_dict
+
+    @classmethod
+    def count(self):
+        '''Returns a count of instance objects'''
+        objects = models.storage.all()
+        count = 0
+
+        for obj in objects:
+            if obj.__class__.__name__ == self.__class__.__name__:
+                count += 1
+        return count
