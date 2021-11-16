403153ae14edc83c40b0e9bbdf636d9852ad6e8c
CamieCoco <75216613+CamieCoco@users.noreply.github.com>
2021-11-17 00:46:00 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""
-Module Amenity class
-"""
+'''
+Defines the Amenity class
+'''
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """
-    Inherits from BaseModel
-    Public class attribute:
-        name: (str)
-    """
-    name = ""
+    '''Amenity for Hbnb application
+    Attributes:
+        name (str): name of the Amenity
+    '''
+
+    name = ''
